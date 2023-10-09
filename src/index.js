9714c1f5ff57c86161d8a27986a4832d0dc06dbd
Anton Arnautov <arnautov.anton@gmail.com>
2023-10-09 15:37:01 +0200

    fix: yet another breaking change
    BREAKING CHANGE: watch out, breaking change incoming

--- src/index.js
+++ src/index.js
@@ -9,3 +9,4 @@ console.log("BREAKING AGAIN!");
 console.log("Release candidate \\w breaking change...");
 console.log("Release candidate \\w another breaking change...");
 console.log("JUST-A-FEATURE!");
+console.log("Release candidate \\w yet another breaking change...");
